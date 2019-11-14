2368856a0aa5c3fc39ad6c2b175a934e050b5ecd
Matt Hall <matt.hall0100@gmail.com>
2019-11-14 00:19:17 -0600

    edits to door.py

--- Libraries/door.py
+++ Libraries/door.py
@@ -4,13 +4,18 @@ import time
 def lock():
 
     #GPIO setup
-    GPIO.setmode(GPIO.BCM)
-    control_pins = [40,38,36,35]
-    powerPin = 37
+    GPIO.setmode(GPIO.BOARD)
+    control_pins = [21,20,16,19]
+    powerPin = 26
 
-    for pin in control_pins:
-        GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, 0)
+    GPIO.setup(control_pins[0], GPIO.OUT)
+    GPIO.output(control_pins[0], 0)
+    GPIO.setup(control_pins[1], GPIO.OUT)
+    GPIO.output(control_pins[1], 0)
+    GPIO.setup(control_pins[2], GPIO.OUT)
+    GPIO.output(control_pins[2], 0)
+    GPIO.setup(control_pins[3], GPIO.OUT)
+    GPIO.output(control_pins[3], 0)
 
     GPIO.setup(powerPin, GPIO.OUT)
     GPIO.output(powerPin,1)
