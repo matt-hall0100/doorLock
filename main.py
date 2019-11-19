2e98b9f42952ea0c8b3f4fc488ab28d6185eab8d
Mitchell Hall <mitchell.hall400@gmail.com>
2019-11-19 00:19:04 -0600

    [dbHandler] Created main event handler

---
+++ main.py
@@ -0,0 +1,22 @@
+from src.dbHandler import getDbLockState, setDbLockState
+from src.door import lock, unlock, getPhysicalState
+
+# Get Initial Physical State
+lockState = getPhysicalState()
+
+# Main Repeated While Loop
+while(True):
+
+    # Database Check
+    tmpLockState = getDbLockState()
+    if tmpLockState != lockState:
+        if tmpLockState:
+            lock()
+        else:
+            unlock()
+        lockState = tmpLockState
+
+    # Physical Check
+    tmpPhysicalState = getPhysicalState()
+    if tmpPhysicalState != lockState:
+        setDbLockState(tmpPhysicalState)
--- src/dbHandler.py
+++ src/dbHandler.py
@@ -2,20 +2,20 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 
-cred = credentials.Certificate('../res/accountKey.json')
+cred = credentials.Certificate('./res/accountKey.json')
 firebase_admin.initialize_app(cred, {'databaseURL': 'https://door-unlock-db303.firebaseio.com'})
 ref = db.reference('lock_state')
 
 
-## setLockState()
+## setDbLockState()
 #  @brief Sets the inputted state to the database lock state
 #  @param Value to set databases state
-def setLockState(state):
+def setDbLockState(state):
     ref.set(state)
 
 
-## getLockState()
+## getDbLockState()
 #  @brief Gets the database lock state
 #  @return Value of databases state
-def getLockState():
+def getDbLockState():
     return ref.get()
